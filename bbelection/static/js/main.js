6ddf62a8aedfbb2703ecedc8df31b6a89c86c5e0
Ben Lardie <benlardie@gmail.com>
2019-03-29 15:20:47 -0400

    finished

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+let body = document.querySelector('body');
+let ul = document.querySelector('ul');
+body.appendChild(ul);
+axios.get('https://bb-election-api.herokuapp.com/')
+.then((response) => {
+    let electionCans = response.data.candidates;
+    electionCans.forEach( person => {
+        console.log(person);
+    let li = document.createElement('li');
+    li.innerHTML = `Name: ${person.name} Votes: ${person.votes}`;
+    ul.appendChild(li);
+    
+});
+});
 
+    
 });
